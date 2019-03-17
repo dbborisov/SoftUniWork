26c31f26994498bac8580e260c910c56e8ddee5b
dborisov <dobromir.borisov24@gmail.com>
2019-03-17 10:43:44 +0200

    All work in home and class till the middle exam.

--- src/MapAndLambda/HomeWork/CompanyUsers.java
+++ src/MapAndLambda/HomeWork/CompanyUsers.java
@@ -9,13 +9,13 @@ public class CompanyUsers {
         Scanner scanner = new Scanner(System.in);
 
         String in = "";
-        Map<String, List<String>> compani = new LinkedHashMap<>();
+        Map<String, List<String>> compani = new TreeMap<>();
 
         while (!"End".equals(in = scanner.nextLine())) {
 
             String[] command = in.split(" -> ");
             String compan = command[0];
-            String name = command[1];
+            String name = command[1].toUpperCase();
             if (!compani.containsKey(compan)) {
                 List<String> employ = new ArrayList<>();
                 employ.add(name);
@@ -31,9 +31,9 @@ public class CompanyUsers {
             }
 
         }
-        compani.entrySet().stream().sorted((e1,e2)->e1.getKey().compareTo(e2.getKey())).forEach(e->{
+        compani.entrySet().stream().forEach(e->{
             System.out.println(String.format("%s",e.getKey()));
-            List<String> students =e.getValue().stream().sorted().collect(Collectors.toList());
+            List<String> students =e.getValue().stream().collect(Collectors.toList());
             int index=e.getValue().size();
             for (int i = 0; i <index ; i++) {
                 System.out.println("-- "+students.get(i));
