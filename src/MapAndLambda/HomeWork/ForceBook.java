acb55f9b796dd665bcd6055ae0f207ce143edaa3
dborisov <dobromir.borisov24@gmail.com>
2019-03-18 14:22:04 +0200

    All work in home and class till the middle exam.

---
+++ src/MapAndLambda/HomeWork/ForceBook.java
@@ -0,0 +1,23 @@
+package MapAndLambda.HomeWork;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ForceBook {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String in = "";
+        Map<String , Set<String>> forceBook = new TreeMap<>();
+
+        while (!"Lumpawaroo".equals(in =scanner.nextLine())){
+
+        }
+
+
+
+    }
+}
+
+
+
