2b9a0cefcb8a5be0dc2446a1fd56483e332e06a9
dborisov <dobromir.borisov24@gmail.com>
2019-03-22 20:21:31 +0200

    All work in home and class till the middle exam.

---
+++ src/Regex/HomeWork/ReplaceRepeatingChars.java
@@ -0,0 +1,15 @@
+package Regex.HomeWork;
+
+import java.util.Scanner;
+
+public class ReplaceRepeatingChars {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+
+        String in = scanner.nextLine();
+        String out ="";
+
+        
+
+    }
+}
