56daf63826c885702517f195c0c0c2805c476ff0
dborisov <dobromir.borisov24@gmail.com>
2019-03-22 10:06:15 +0200

    All work in home and class till the middle exam.

---
+++ src/Regex/HomeWork/ValidUserName.java
@@ -0,0 +1,22 @@
+package Regex.HomeWork;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ValidUserName {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        String[] out = input.split(",\\s+");
+        System.out.println();
+        Arrays.stream(out).forEach(e->{
+            if(e.matches("[A-Za-z0-9-_]{3,16}")){
+                System.out.println(e);
+            }
+        });
+
+    }
+
+}
---
+++ src/Regex/demo.java
@@ -0,0 +1,21 @@
+package Regex;
+
+import java.time.Instant;
+import java.util.Scanner;
+
+public class demo {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+         Instant start =  Instant.now();
+        System.out.println(start);
+
+         Instant stop = Instant.now();
+        System.out.println(stop);
+
+        String[] arr = scanner.nextLine().split("\\s+");
+        System.out.println("texttextText".replaceAll("text", ""));
+
+    }
+}
