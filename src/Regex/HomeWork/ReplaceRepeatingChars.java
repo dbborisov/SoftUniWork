953c89e5515fdebce7c91b0e93505280d047369c
dborisov <dobromir.borisov24@gmail.com>
2019-03-23 13:29:04 +0200

    All work in home and class till the middle exam.

--- src/Regex/HomeWork/ReplaceRepeatingChars.java
+++ src/Regex/HomeWork/ReplaceRepeatingChars.java
@@ -4,12 +4,41 @@ import java.util.Scanner;
 
 public class ReplaceRepeatingChars {
     public static void main(String[] args) {
-        Scanner scanner =new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         String in = scanner.nextLine();
-        String out ="";
+//        in=in.replaceAll("\\s+","");
+        String out = "";
+        int count = 0;
 
-        
+        for (int i = 0; i < in.length(); i++) {
+            char cha = in.charAt(i);
+
+            for (int j = i; j < in.length(); j++) {
+
+                if (cha == in.charAt(j)) {
+                    count++;
+
+                } else {
+                    out += cha;
+                    in = in.substring(j);
+                    count = 0;
+                    i--;
+                    break;
+                }
+            }
+
+        }
+        if (in.length() == 2) {
+            if (in.charAt(0) == in.charAt(1)) {
+                out += in.charAt(0);
+            }
+        } else {
+            if (in.length() == 1) {
+                out += in.charAt(0);
+            }
+        }
+        System.out.println(out);
 
     }
 }
