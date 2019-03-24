21f46beab6e9d91dac36dca5457e64783117e10c
dborisov <dobromir.borisov24@gmail.com>
2019-03-24 16:06:20 +0200

    All work in home and class till the middle exam.

---
+++ src/Regex/HomeWork/Extract_Emails.java
@@ -0,0 +1,13 @@
+package Regex.HomeWork;
+
+import java.util.Scanner;
+
+public class Extract_Emails {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String in = scanner.nextLine();
+
+        String[] out = in.split("");
+
+    }
+}
