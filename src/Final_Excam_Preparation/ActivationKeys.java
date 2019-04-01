d174204d1b83384515ce2ce951f5052e2afd91e0
dborisov <dobromir.borisov24@gmail.com>
2019-04-01 15:13:22 +0300

    All work in home and class till the middle exam.

---
+++ src/Final_Excam_Preparation/ActivationKeys.java
@@ -0,0 +1,61 @@
+package Final_Excam_Preparation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class ActivationKeys {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String[] in = scanner.nextLine().split("&");
+
+        Pattern pattern = Pattern.compile("(?:&)*([a-zA-Z0-9]{16,25})(?:&)*");
+        List<String> ok = new ArrayList<>();
+        for (int i = 0; i <in.length ; i++) {
+            Matcher matcher = pattern.matcher(in[i]);
+            if(matcher.find()){
+                ok.add(in[i]);
+            }
+
+        }
+
+        for (int i = 0; i <ok.size() ; i++) {
+            String newOk = "";
+            if(ok.get(i).length()==16){
+                for (int j = 0; j <ok.get(i).length() ; j++) {
+                    if(j%4==0){
+                        newOk+="-";
+                    }
+
+                    if(ok.get(i).charAt(j)>=48&&ok.get(i).charAt(j)<=57){
+
+                        newOk+=(9 - Integer.parseInt(ok.get(i).charAt(j)+"")+"");
+                        continue;
+                    }
+                    newOk+=ok.get(i).charAt(j);
+                }
+            }else{
+                for (int j = 0; j <ok.get(i).length() ; j++) {
+                    if(j%5==0){
+                        newOk+="-";
+                    }
+                    if(ok.get(i).charAt(j)>=48&&ok.get(i).charAt(j)<=57){
+
+                        newOk+=(9 - Integer.parseInt(ok.get(i).charAt(j)+"")+"");
+                        continue;
+                    }
+                    newOk+=ok.get(i).charAt(j);
+                }
+            }
+            ok.set(i,newOk.substring(1).toUpperCase());
+
+        }
+        String out = ok.stream().collect(Collectors.joining(", "));
+        System.out.println(out);
+
+    }
+}
