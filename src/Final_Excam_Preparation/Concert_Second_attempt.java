f5bb22e65b64bd4dac7a73116a9815e67825f938
dborisov <dobromir.borisov24@gmail.com>
2019-04-02 14:17:29 +0300

    All work in home and class till the middle exam.

---
+++ src/Final_Excam_Preparation/Concert_Second_attempt.java
@@ -0,0 +1,11 @@
+package Final_Excam_Preparation;
+
+import java.util.Scanner;
+
+public class Concert_Second_attempt {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
