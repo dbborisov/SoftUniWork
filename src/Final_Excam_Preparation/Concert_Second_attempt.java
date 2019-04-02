0dacdd6faa8adfe323de94481014c7ae1498b467
dborisov <dobromir.borisov24@gmail.com>
2019-04-02 15:32:24 +0300

    All work in home and class till the middle exam.

--- src/Final_Excam_Preparation/Concert_Second_attempt.java
+++ src/Final_Excam_Preparation/Concert_Second_attempt.java
@@ -7,61 +7,54 @@ public class Concert_Second_attempt {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String in = "";
-        Map<String,List<String>> bandMember = new LinkedHashMap<>();
-        Map<String,Integer> bandTime = new LinkedHashMap<>();
+        Map<String, List<String>> bandMember = new LinkedHashMap<>();
+        Map<String, Integer> bandTime = new LinkedHashMap<>();
 
-        while (!"start of concert".equals(in=scanner.nextLine())){
+        while (!"start of concert".equals(in = scanner.nextLine())) {
             String[] command = in.split(";\\s+");
-            String bandName =command[1];
+            String bandName = command[1];
             List<String> inBand = new ArrayList<>();
             int time = 0;
-             if(command[0].equals("Add")){
-                 inBand = Arrays.stream(command[2].split(",\\s+")).collect(Collectors.toList());
-                 if(!bandMember.containsKey(bandName)){
-                     bandMember.put(bandName,inBand);
-                     bandTime.put(bandName,0);
-                 }else{
-                     for (int i = 0; i <inBand.size() ; i++) {
-                         if(!bandMember.get(bandName).contains(inBand.get(i))){
-                             bandMember.get(bandName).add(inBand.get(i));
-                         }
+            if (command[0].equals("Add")) {
+                inBand = Arrays.stream(command[2].split(",\\s+")).collect(Collectors.toList());
+                if (!bandMember.containsKey(bandName)) {
+                    bandMember.put(bandName, inBand);
+                    bandTime.put(bandName, 0);
+                } else {
+                    for (String s : inBand) {
+                        if (!bandMember.get(bandName).contains(s)) {
+                            bandMember.get(bandName).add(s);
+                        }
 
-                     }
-                 }
+                    }
+                }
 
-             }else if(command[0].equals("Play")){
-                 time=Integer.parseInt(command[2]);
+            } else if (command[0].equals("Play")) {
+                time = Integer.parseInt(command[2]);
 
-                 if(!bandTime.containsKey(bandName)){
-                     bandMember.put(bandName,new ArrayList<>());
-                     bandTime.put(bandName,time);
-                 }else{
-                     bandTime.put(bandName,bandTime.get(bandName)+time);
-                 }
+                if (!bandTime.containsKey(bandName)) {
+                    bandMember.put(bandName, new ArrayList<>());
+                    bandTime.put(bandName, time);
+                } else {
+                    bandTime.put(bandName, bandTime.get(bandName) + time);
+                }
 
-             }
-
+            }
         }
         in = scanner.nextLine();
 
-        System.out.println("Total time: "+bandTime.values().stream().mapToInt(Integer::intValue).sum());
-        bandTime.entrySet().stream().sorted((e1,e2)->{
+        System.out.println("Total time: " + bandTime.values().stream().mapToInt(Integer::intValue).sum());
+        bandTime.entrySet().stream().sorted((e1, e2) -> {
             int sum = e2.getValue().compareTo(e1.getValue());
-            if(sum==0){
+            if (sum == 0) {
                 sum = e1.getKey().compareTo(e2.getKey());
             }
             return sum;
-        }).forEach(e->{
-            System.out.println(String.format("%s -> %d",e.getKey(),e.getValue()));
+        }).forEach(e -> {
+            System.out.println(String.format("%s -> %d", e.getKey(), e.getValue()));
         });
         System.out.println(in);
-        bandMember.get(in).stream().forEach(e->{
-            System.out.println(String.format("=> %s",e));
-        });
-
-
-
-
+        bandMember.get(in).forEach(e -> System.out.println(String.format("=> %s", e)));
     }
 
 }
---
+++ src/Final_Excam_Preparation/VaporWinterSale_2.java
@@ -0,0 +1,11 @@
+package Final_Excam_Preparation;
+
+import java.util.Scanner;
+
+public class VaporWinterSale_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
