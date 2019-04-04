ab3ea250be7f7f315fea9c290018cc81ef65de9c
dborisov <dobromir.borisov24@gmail.com>
2019-04-04 15:45:00 +0300

    All my work in Java Advanced and Java OOP

---
+++ src/Stakcs_And_Queues/Lection/DecimalToBinary.java
@@ -0,0 +1,9 @@
+package Stakcs_And_Queues.Lection;
+
+import java.util.Scanner;
+
+public class DecimalToBinary {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
---
+++ src/Stakcs_And_Queues/Lection/Simple_Calculator.java
@@ -0,0 +1,27 @@
+package Stakcs_And_Queues.Lection;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class Simple_Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayDeque<Integer> sum = new ArrayDeque<>();
+        String[] in =scanner.nextLine().split(" ");
+
+        for (int i = 2; i <in.length ; i+=2) {
+
+            if(i==2) {
+                sum.push(Integer.parseInt(in[i - 2]));
+            }
+            if(in[i-1].equals("+")){
+                sum.push(sum.pop()+Integer.parseInt(in[i]));
+            }else{
+                sum.push(sum.pop()-Integer.parseInt(in[i]));
+            }
+
+        }
+        System.out.println(sum.pop());
+    }
+}
