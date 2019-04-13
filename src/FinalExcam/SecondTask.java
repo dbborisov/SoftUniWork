f7369fe143347454a995173ebe426ec109603c29
dborisov <dobromir.borisov24@gmail.com>
2019-04-13 09:38:53 +0300

    All work in home and class till the middle exam.

---
+++ src/FinalExcam/SecondTask.java
@@ -0,0 +1,11 @@
+package FinalExcam;
+
+import java.util.Scanner;
+
+public class SecondTask {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String in = scanner.nextLine();
+    }
+}
