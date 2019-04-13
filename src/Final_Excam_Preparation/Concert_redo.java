528f9d4bbfc2f33a164244ac8b0169932762c6f3
dborisov <dobromir.borisov24@gmail.com>
2019-04-13 16:04:51 +0300

    All work in home and class till the middle exam.

---
+++ src/Final_Excam_Preparation/Concert_redo.java
@@ -0,0 +1,12 @@
+package Final_Excam_Preparation;
+
+import java.util.Scanner;
+
+public class Concert_redo {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
