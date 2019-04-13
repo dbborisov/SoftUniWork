642fe65361b99619a12d4f423afbe7b4a9ae9b86
dborisov <dobromir.borisov24@gmail.com>
2019-04-13 13:26:12 +0300

    All work in home and class till the middle exam.

---
+++ src/Final_Excam_Preparation/Activation_Keys.java
@@ -0,0 +1,10 @@
+package Final_Excam_Preparation;
+
+import java.util.Scanner;
+
+public class Activation_Keys {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
